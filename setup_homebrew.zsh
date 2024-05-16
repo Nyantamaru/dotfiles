adc2be42a88834c1077ca36d90b695293579ba24
Manuel Cruz <5506079+Nyantamaru@users.noreply.github.com>
2024-05-16 16:04:22 -0600

    Add setup_homebrew.zsh and run Dotbot Install Homebrew packages: httpie, bat

---
+++ setup_homebrew.zsh
@@ -0,0 +1,12 @@
+#!/usr/bin/env zsh
+
+echo "\n<<< Starting Hombrew Setup >>>\n"
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.zshrc
+
+brew install httpie
+brew install  bat
