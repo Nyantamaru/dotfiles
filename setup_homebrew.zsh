a3f2754034939fd91d0a9037fafea47c2e41f43c
Manuel Cruz <5506079+Nyantamaru@users.noreply.github.com>
2024-05-16 18:23:08 -0600

    Switch to Brewfile for Homebrew and install exa.

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -4,9 +4,11 @@ echo "\n<<< Starting Hombrew Setup >>>\n"
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
-test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.zshrc
+# test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+# test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+# echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.zshrc
 
-brew install httpie
-brew install  bat
+# brew install httpie
+# brew install  bat
+
+brew bundle --verbose
